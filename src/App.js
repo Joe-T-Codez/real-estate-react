0afabca9ecaddc4d96ca4b4b1e5d2cd2e26c2078
Joe Tejeda <mercyprime70@gmail.com>
2020-03-19 19:02:23 -0400

    header part

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import highrise from './img/highrise.jpg';
+import React, { Component} from 'react'
+import Header from './Header.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> REAL ESTATE</h1>
-      <img src={highrise} alt=""></img>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+      <Header/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React, { Component} from 'react'
+// import ReactDOM from 'react-dom'
+
+
+export default class Header extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+    }
+  }
+  render () {
+    return (<header>
+      <div className="logo"> Jone's Real Estate</div>
+
+      <nav className="menu-links">
+      <a href="https://www.youtube.com/">Create Ads</a>
+      <a href="https://www.youtube.com/">About Us</a>
+      <a href="https://www.youtube.com/">Log In</a>
+      <a href="https://www.youtube.com/" className="register-btn">Register </a>
+      </nav>
+      </header>)
+  }
+}
