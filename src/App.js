1b543b8b6a8abbbf7e1f6d2a5884f8840e230602
Joe Tejeda <mercyprime70@gmail.com>
2020-03-19 21:26:58 -0400

    continuation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,151 @@
+
 import React, { Component} from 'react'
 import Header from './Header.js'
 import Filter from './Filter.js'
+import Listings from './Listings.js'
+import listingsData from './data/listingsData.js'
 import './App.css';
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
+export default class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe',
+      listingsData,
+      min_price:0,
+      max_price:10000000,
+      city:'All',
+      homeType:'All',
+      bedrooms:'0',
+      min_floorspace:0,
+      max_floorspace:50000,
+      elevator:false,
+      gym:false,
+      fireplace:false,
+      swimming_pool:false,
+      filteredData:listingsData,
+      populateFormsData:'',
+      sortby:'price-dsc',
+      view:'long',
+      search:''
+    }
+    this.change=this.change.bind(this)
+    this.filteredData=this.filteredData.bind(this)
+    this.populateForms=this.populateForms.bind(this)
+    this.changeView=this.changeView.bind(this)
+  }
+  componentWillMount(){
+
+var listingsData= this.state.listingsData.sort((a,b)=>{
+  return a.price - b.price
+})
+
+    this.setState({
+      listingsData
+    })
+  }
+  change(event){
+    var name=event.target.name
+    var value=(event.target.type==='checkbox') ? event.target.checked: event.target.value
+console.log(listingsData.city)
+    this.setState({
+      [name]:value
+    }, ()=>{
+      console.log(this.state)
+      this.filteredData()
+    })
+  }
+  changeView(viewName){
+    this.setState({
+      view:viewName
+    })
+  }
+  filteredData(){
+    var newData=this.state.listingsData.filter((item) =>{
+      return item.price >=this.state.min_price && item.price<=
+      this.state.max_price && item.floorSpace >= this.state.min_floorspace
+      && item.floorSpace <= this.state.max_floorspace && item.rooms >=this.state.bedrooms
+    })
+
+      if (this.state.city !=="All"){
+        newData=newData.filter((item) =>{
+          return item.city === this.state.city
+        })
+      }
+
+      if (this.state.homeType !=="All"){
+        newData=newData.filter((item) =>{
+          return item.homeType === this.state.homeType
+        })
+      }
+
+      if(this.state.sortby=== 'price-dsc'){
+        newData= newData.sort((a,b)=>{
+          return a.price-b.price
+        })
+      }
+      if(this.state.sortby=== 'price-asc'){
+        newData= newData.sort((a,b)=>{
+          return b.price-a.price
+        })
+      }
+      if(this.state.sortby=== 'floorspace-asc'){
+        newData= newData.sort((a,b)=>{
+          return a.floorSpace-b.floorSpace
+        })
+      }
+      if(this.state.sortby=== 'floorspace-dsc'){
+        newData= newData.sort((a,b)=>{
+          return b.floorSpace-a.floorSpace
+        })
+      }
+
+    this.setState({
+      filteredData:newData
+    })
+  }
+
+  populateForms(){
+    //city
+var cities=this.state.listingsData.map((item)=>{
+  return item.city
+})
+cities = new Set(cities)
+cities =[...cities]
+cities= cities.sort()
+
+    //homeType
+    var homeTypes=this.state.listingsData.map((item)=>{
+      return item.homeType
+    })
+    homeTypes = new Set(homeTypes)
+    homeTypes=[...homeTypes]
+    homeTypes=homeTypes.sort()
+    //bedrooms
+    var bedrooms=this.state.listingsData.map((item)=>{
+      return item.rooms
+    })
+    bedrooms = new Set(bedrooms)
+    bedrooms=[...bedrooms]
+    bedrooms=bedrooms.sort()
+
+    this.setState({
+      populateFormsData:{
+        homeTypes,
+        bedrooms,
+        cities
+      }
+    }, ()=>{
+      // console.log(this.state)
+    })
+  }
+  render () {
+    return (<div>
       <Header/>
-      <Filter/>
-      </div>
-    );
+      <section id="content-area">
+      <Filter change={this.change} globalState={this.state} populateAction={this.populateForms}/>
+      <Listings listingsData={this.state.filteredData} change={this.change} globalState={this.state} changeView={this.changeView}/>
+      </section>
+      </div>)
   }
 }
-
-export default App;
---
+++ src/Listings.js
@@ -0,0 +1,135 @@
+import React, { Component} from 'react'
+
+
+
+export default class Listings extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+    }
+    this.loopListings=this.loopListings.bind(this)
+  }
+  loopListings (){
+var {listingsData}=this.props
+    if(listingsData === undefined || listingsData.length===0){
+      return "Sorry your filter did not match any listing"
+    }
+    return listingsData.map((listing, index)=>{
+    if (this.props.globalState.view === 'long'){
+      //This is box view
+      return(<div className="col-md-3" key={index}>
+        <div className="listing-info">
+          <div className="listing-image" style={{background:`url("${listing.image}") no-repeat center center`}}>
+            <span className="address">{listing.address}</span>
+          <div className="details">
+            <div className="col-md-3">
+            <div className="user-image"></div>
+            </div>
+        <div className="user-details">
+          <span className="user-name">Martin Smith</span>
+          <span className="post-date">Posted on 05/09/18</span>
+        </div>
+        <div className="col-md-9">
+        <div className="listing-details">
+          <div className="floorspace"><i className="far fa-square"></i>
+          <span>{listing.floorSpace} ft&sup2;</span>
+          </div>
+          <div className="bedrooms">
+          <i className="fas fa-bed"></i>
+          <span>{listing.rooms}</span>
+          </div>
+          </div>
+          <div className="view-btn">
+            View Listing
+          </div>
+
+          </div>
+          </div>
+      </div>
+      <div className="bottom-info">
+      <span className="price">${listing.price}</span>
+      <span className="location"><i className="fas fa-map-marker-alt"></i>{listing.city}{listing.state}</span>
+      </div>
+        </div>
+        </div>)}
+        else {
+          //This is long view
+          return(<div className="col-md-12 col-lg-6" key={index}>
+            <div className="listing-info" key={index}>
+              <div className="listing-image" style={{background:`url("${listing.image}") no-repeat center center`}}>
+                <span className="address">{listing.address}</span>
+              <div className="details">
+              <div className="col-md-3">
+              <div className="user-image"></div>
+              </div>
+                <div className="col-md-9">
+            <div className="user-details">
+              <span className="user-name">Martin Smith</span>
+              <span className="post-date">Posted on 05/09/18</span>
+            </div>
+            <div className="listing-details">
+              <div className="floorspace">
+              <i className="far fa-square"></i>
+              <span>{listing.floorSpace} ft&sup2;</span>
+              </div>
+              <div className="bedrooms">
+              <i className="fas fa-bed"></i>
+              <span>{listing.rooms}</span>
+              </div>
+              </div>
+              <div className="view-btn">
+                View Listing
+              </div>
+              </div>
+              </div>
+          </div>
+          <div className="bottom-info">
+          <span className="price">${listing.price}</span>
+          <span className="location"><i className="fas fa-map-marker-alt"></i>{listing.city}{listing.state}</span>
+          </div>
+            </div>
+          </div>)
+        }
+        })
+}
+  render () {
+    return (<section id="listings">
+    <section className="search-area">
+    <input type="text" name="search" onChange={this.props.change}/>
+    </section>
+    <section className="sortby-area">
+      <div className="result-nums"> {this.props.globalState.filteredData.length} results found</div>
+      <div className="sort-options">
+      <select name="sortby" className="sortby" onChange={this.props.change}>
+      <option value="price-dsc">Price Ascending Order</option>
+      <option value="price-asc">Price Descending Order</option>
+      <option value="floorspace-asc">Floor Space Ascending Order</option>
+      <option value="floorspace-dsc">Floor Space Descending Order</option>
+      </select>
+
+      <div className="sort-icon">
+      <i className="fas fa-th-list" onClick={this.props.changeView.bind(null,"box")}></i>
+      <i className="fas fa-th" onClick={this.props.changeView.bind(null,"long")}></i>
+      </div>
+      </div>
+    </section>
+
+    <section className="listings-results">
+    <div className="row">
+      {this.loopListings()}
+      </div>
+    </section>
+    <section id="pagination">
+    <ul className="pages">
+      <li>Prev</li>
+      <li className="active">1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>Next</li>
+    </ul>
+    </section>
+    </section>)
+  }
+}
